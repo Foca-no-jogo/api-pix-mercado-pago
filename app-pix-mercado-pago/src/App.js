44540fb4714eabbc3c8faa309366a21f383935c4
Foca-no-jogo <c.andradeinacio@gmail.com>
2024-10-25 10:55:28 -0300

    first commit

---
+++ app-pix-mercado-pago/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">              
+         PIX com API do mercado pago        
+      </header>
+    </div>
+  );
+}
+
+export default App;
